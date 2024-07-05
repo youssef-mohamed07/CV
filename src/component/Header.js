87f73211bd1311f7e16908cf36ddae85307275bb
Carbona777 <youssefmohamedahmed2004@gmail.com>
2024-07-05 18:59:09 +0300

    edit

--- src/component/Header.js
+++ src/component/Header.js
@@ -3,13 +3,13 @@ import { FaLinkedin, FaGithub, FaTwitter, FaSun, FaMoon, FaFacebook, FaInstagram
 import { motion } from 'framer-motion';
 
 const Header = ({ toggleDarkMode, darkMode }) => (
-  <header className={`bg-gradient-to-r ${darkMode ? 'from-gray-800 to-gray-900' : 'from-blue-500 to-purple-600'} text-white py-8 shadow-lg`}>
-    <div className="container mx-auto flex justify-between items-center">
-      <motion.div initial={{ y: -250 }} animate={{ y: 0 }} transition={{ type: 'spring', stiffness: 120 }}>
-        <h1 className="text-5xl font-bold">Youssef Mohamed</h1>
-        <p className="text-2xl">Software Engineer</p>
+  <header className={`bg-gradient-to-r ${darkMode ? 'from-gray-800 to-gray-900' : 'from-blue-500 to-purple-600'} text-white py-4 shadow-lg`}>
+    <div className="container mx-auto flex flex-col md:flex-row justify-between items-center px-4">
+      <motion.div initial={{ y: -250 }} animate={{ y: 0 }} transition={{ type: 'spring', stiffness: 120 }} className="text-center md:text-left">
+        <h1 className="text-3xl md:text-5xl font-bold">Youssef Mohamed</h1>
+        <p className="text-xl md:text-2xl">Software Engineer</p>
       </motion.div>
-      <motion.div className="flex space-x-4" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 1 }}>
+      <motion.div className="flex space-x-4 mt-4 md:mt-0" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 1 }}>
         <a href="https://www.linkedin.com/in/youssef-mohamed-96158330a" target="_blank" rel="noopener noreferrer" className="hover:text-gray-200">
           <FaLinkedin size="2em" />
         </a>
@@ -25,7 +25,7 @@ const Header = ({ toggleDarkMode, darkMode }) => (
         <a href="https://www.instagram.com/youssef_mohamed7100?igsh=YzljYTk1ODg3Zg==" target="_blank" rel="noopener noreferrer" className="hover:text-gray-200">
           <FaInstagram size="2em" />
         </a>
-        <button onClick={toggleDarkMode} className="focus:outline-none">
+        <button onClick={toggleDarkMode} className="focus:outline-none hover:text-gray-200">
           {darkMode ? <FaSun size="2em" /> : <FaMoon size="2em" />}
         </button>
       </motion.div>
